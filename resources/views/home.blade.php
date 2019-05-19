8b86209a37761544c37322f8a0a19ac21532e001
Daniyal <daniyaladmaney@gmail.com>
2019-05-19 17:46:42 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,87 +14,58 @@
                     @endif
                     You are logged in!
                     <section class = "row new-post">
-                        <div class = "col-md-6 col-md-offset-3">
+                        <div class = "col-md-12 col-md-offset-3">
                             <header><h3>What do you want to post?</h3> </header>
                             <form action = "">
                                 <div class ="form-group">
-                                    <textarea class = "form-control" name = "new-post" id = "new-post" rows = 5 placeholder="PLACEHOLDER"></textarea>
+                                    <textarea class = "form-control" name = "new-post" id = "new-post" rows = 5 placeholder="What's on your mind?"></textarea>
+                                    <div class = "col-md-9 col-md-offset-3">
+                                    <label for="postPic" class=" col-form-label text-md-right">{{ __('Upload Image') }}</label>
+                                     <input id="postPic" type = "file" name = "post_pic" class="form-control @error('Post Picture') is-invalid @enderror" name="postPic" value="{{ old('postPic') }}" autocomplete="postPic"> 
+
+
+                                     @error('postPic')
+                                     <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
-                                <button type = "submit" class = "btn btn-primary">New Post</button>
-                            </form>
-                        </div>
-                    </section>
-                    <section class = "row posts">
-                        <div class = "col-md-6 col-md-offset-3">
-                            <header><h3>Posts from other people...</h3></header>
-                            <article class = "post">
+                            </div>
+                            <button type = "submit" class = "btn btn-primary">New Post</button>
+                        </form>
+                    </div>
+                </section>
+                <section class = "row posts">
+                    <div class = "col-md-6 col-md-offset-3">
+                        <header><h3>Posts from other people...</h3></header>
+                        <article class = "post">
+                            <div class = "info">
+                                Posted by: Daniyal on 19 May 2019 at 22:05
+                            </div>
+                            <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>      
+                            <div class = "interaction"> >
+                                <a href = "#"> upvote </a> -
+                                <a href = "#"> downvote </a> -
+                                <a href = "#"> reply </a> -
+                                <a href = "#"> edit </a> -
+                                <a href = "#"> delete </a> <
+                            </div>
+                        </article>
+                        <article class = "post">
+                            <div class = "info">
+                                Posted by: Faran on 19 May 2019 at 10:05
+                            </div>
+                            <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>
+                            <div class = "interaction"> >
+                                <a href = "#"> upvote </a> -
+                                <a href = "#"> downvote </a> -
+                                <a href = "#"> reply </a> -
+                                <a href = "#"> edit </a> -
+                                <a href = "#"> delete </a> <
+                            </div>
+                            <article class = "reply">
                                 <div class = "info">
-                                    Posted by: Daniyal on 19 May 2019 at 22:05
-                                </div>
-                                <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>      
-                                <div class = "interaction"> >
-                                    <a href = "#"> upvote </a> -
-                                    <a href = "#"> downvote </a> -
-                                    <a href = "#"> reply </a> -
-                                    <a href = "#"> edit </a> -
-                                    <a href = "#"> delete </a> <
-                                </div>
-                            </article>
-                            <article class = "post">
-                                <div class = "info">
-                                    Posted by: Faran on 19 May 2019 at 10:05
-                                </div>
-                                <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>
-                                <div class = "interaction"> >
-                                    <a href = "#"> upvote </a> -
-                                    <a href = "#"> downvote </a> -
-                                    <a href = "#"> reply </a> -
-                                    <a href = "#"> edit </a> -
-                                    <a href = "#"> delete </a> <
-                                </div>
-                                <article class = "reply">
-                                    <div class = "info">
-                                        Replied by: Faran on 19 May 2019 at 10:30
-                                    </div>
-                                    <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>
-                                    <div class = "interaction"> >
-                                        <a href = "#"> upvote </a> -
-                                        <a href = "#"> downvote </a> -
-                                        <a href = "#"> reply </a> -
-                                        <a href = "#"> edit </a> -
-                                        <a href = "#"> delete </a> <
-                                    </div>
-                                </article>
-                            </article>
-                            <article class = "post">
-                                <div class = "info">
-                                    Posted by: Isa on 18 May 2019 at 10:20
-                                </div>
-                                <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>                           
-                                <div class = "interaction"> >
-                                    <a href = "#"> upvote </a> -
-                                    <a href = "#"> downvote </a> -
-                                    <a href = "#"> reply </a> -
-                                    <a href = "#"> edit </a> -
-                                    <a href = "#"> delete </a> <
-                                </div>
-                            </article>
-                            <article class = "post">
-                                <div class = "info">
-                                    Posted by: Serkan on 18 May 2019 at 12:00
-                                </div>
-                                <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>           
-                                <div class = "interaction"> >
-                                    <a href = "#"> upvote </a> -
-                                    <a href = "#"> downvote </a> -
-                                    <a href = "#"> reply </a> -
-                                    <a href = "#"> edit </a> -
-                                    <a href = "#"> delete </a> <
-                                </div>
-                            </article>
-                            <article class = "post">
-                                <div class = "info">
-                                    Posted by: Admaney on 16 May 2019 at 22:05
+                                    Replied by: Faran on 19 May 2019 at 10:30
                                 </div>
                                 <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>
                                 <div class = "interaction"> >
@@ -105,9 +76,49 @@
                                     <a href = "#"> delete </a> <
                                 </div>
                             </article>
-                        </div>
+                        </article>
+                        <article class = "post">
+                            <div class = "info">
+                                Posted by: Isa on 18 May 2019 at 10:20
+                            </div>
+                            <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>                           
+                            <div class = "interaction"> >
+                                <a href = "#"> upvote </a> -
+                                <a href = "#"> downvote </a> -
+                                <a href = "#"> reply </a> -
+                                <a href = "#"> edit </a> -
+                                <a href = "#"> delete </a> <
+                            </div>
+                        </article>
+                        <article class = "post">
+                            <div class = "info">
+                                Posted by: Serkan on 18 May 2019 at 12:00
+                            </div>
+                            <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>           
+                            <div class = "interaction"> >
+                                <a href = "#"> upvote </a> -
+                                <a href = "#"> downvote </a> -
+                                <a href = "#"> reply </a> -
+                                <a href = "#"> edit </a> -
+                                <a href = "#"> delete </a> <
+                            </div>
+                        </article>
+                        <article class = "post">
+                            <div class = "info">
+                                Posted by: Admaney on 16 May 2019 at 22:05
+                            </div>
+                            <p> Cattle them herb there bearing tree great had days man own divided after i, brought. Fish i blessed. Lesser moved.</p>
+                            <div class = "interaction"> >
+                                <a href = "#"> upvote </a> -
+                                <a href = "#"> downvote </a> -
+                                <a href = "#"> reply </a> -
+                                <a href = "#"> edit </a> -
+                                <a href = "#"> delete </a> <
+                            </div>
+                        </article>
                     </div>
                 </div>
             </div>
         </div>
-        @endsection
+    </div>
+    @endsection
